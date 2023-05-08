83db04b424d8c5bc17398d97c3b23cb9197a3d82
Jisuk <sue030124@naver.com>
2023-05-08 19:46:05 +0900

    홈페이지 업데이트

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    img = models.ImageField(null=True, upload_to="images/", blank=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -16,4 +17,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return str(self.title)
